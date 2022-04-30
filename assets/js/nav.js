b12d22997885c0601255cd87247a8551c95148df
Amanjot726 <67585828+Amanjot726@users.noreply.github.com>
2022-04-30 17:17:41 +0530

    Initial commit

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,27 +1,7 @@
-function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    /*loop through a collection of all HTML elements:*/
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-      elmnt = z[i];
-      /*search for elements with a certain atrribute:*/
-      file = elmnt.getAttribute("w3-include-html");
-      if (file) {
-        /*make an HTTP request using the attribute value as the file name:*/
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4) {
-            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-            /*remove the attribute, and call this function once more:*/
-            elmnt.removeAttribute("w3-include-html");
-            includeHTML();
-          }
-        }      
-        xhttp.open("GET", file, true);
-        xhttp.send();
-        /*exit the function:*/
-        return;
-      }
-    }
-  };
+fetch('https://raw.githubusercontent.com/Amanjot726/cml.gndec/main/a/Header.html')
+.then(res => res.text())
+.then(text => {
+    let ele = document.querySelector(".load");
+    ele.innerHTML = text;
+    // oldelem.parentNode.replaceChild(newelem,oldelem);
+})
