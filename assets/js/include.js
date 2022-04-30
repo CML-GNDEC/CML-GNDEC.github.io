7c88e196678f9e80b8f31501bf43250864bda307
Amanjot726 <67585828+Amanjot726@users.noreply.github.com>
2022-04-30 18:28:35 +0530

    Initial commit

--- assets/js/include.js
+++ assets/js/include.js
@@ -6,10 +6,10 @@
 //     // oldelem.parentNode.replaceChild(newelem,oldelem);
 // })
 
-fetch('https://raw.githubusercontent.com/Amanjot726/cml.gndec/main/a/footer.html')
-.then(res => res.text())
-.then(text => {
-    let ele = document.querySelector(".fetch-footer");
-    ele.innerHTML = text;
-    // oldelem.parentNode.replaceChild(newelem,oldelem);
-})
+// fetch('https://raw.githubusercontent.com/Amanjot726/cml.gndec/main/a/footer.html')
+// .then(res => res.text())
+// .then(text => {
+//     let ele = document.querySelector(".fetch-footer");
+//     ele.innerHTML = text;
+//     // oldelem.parentNode.replaceChild(newelem,oldelem);
+// })
